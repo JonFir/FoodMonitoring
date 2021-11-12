b9827091f2b4a5deaf154ea5775d17f9aa4204c9
jonfir <yolchev@gmsil.com>
2021-11-12 17:59:25 +0700

    first

---
+++ FoodMonitoring/FoodMonitoring/FoodMonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMonitoringApp.swift
+//  FoodMonitoring
+//
+//  Created by Евгений Елчев on 12.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodMonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

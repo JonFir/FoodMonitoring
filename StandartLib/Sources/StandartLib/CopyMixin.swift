853403e6e7ae373e0475020d5913cc3797b3f373
jonfir <yolchev@gmsil.com>
2021-12-24 17:02:20 +0700

    format code in CopyMixin

--- StandartLib/Sources/StandartLib/CopyMixin.swift
+++ StandartLib/Sources/StandartLib/CopyMixin.swift
@@ -1,13 +1,15 @@
 public protocol CopyMixin {
-    typealias Transformer  = ( _ object: inout Self) throws -> Void
+    typealias Transformer = (_ object: inout Self) throws -> Void
     
     func copy(transformer: Transformer) rethrows -> Self
 }
 
 public extension CopyMixin {
+    
     func copy(transformer: Transformer) rethrows -> Self {
         var copy = self
         try transformer(&copy)
         return copy
     }
+    
 }
